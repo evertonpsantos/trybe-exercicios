d5819e404804bb3c668feb3813e746ca56ce3fd7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-12 19:12:53 -0300

    Adiciona test.js

--- fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/test.js
+++ fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/test.js
@@ -1,10 +1,10 @@
-const notaDesafioPratico = 2;
+const notaDesafioPratico = 60;
 
 if (notaDesafioPratico >= 80 && notaDesafioPratico <= 100) {
    console.log("Parabéns, você foi aprovado!!")
 } else if (notaDesafioPratico < 80 && notaDesafioPratico >= 60) {
    console.log("Você esta na nossa lista de espera.")
-} else if (notaDesafioPratico < 60 && notaDesafioPratico >= 0) {
+} else if (notaDesafioPratico < 60 && notaDesafioPratico <= 0) {
    console.log("Você foi reprovado.")
 } else {
    console.log("Valor inválido.")

6570b272ae395081776978a6fc7d6517cf1180a4
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-13 19:35:08 -0300

    E6

--- fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.2/e6.js
+++ fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.2/e6.js
@@ -2,13 +2,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let oddNumbers = [];
 
 for (let i = 0; i < numbers.length; i += 1) {
-   if (numbers[i] % 2 !== 0) {
-      oddNumbers.push(numbers[i]);
-   }
+  if (numbers[i] % 2 !== 0) {
+    oddNumbers.push(numbers[i]);
+  }
 }
 console.log(oddNumbers.length);
 
 if (oddNumbers === 0) {
-   console.log ("Nenhum valor ímpar encontrado");
+  console.log("Nenhum valor ímpar encontrado");
 }
-

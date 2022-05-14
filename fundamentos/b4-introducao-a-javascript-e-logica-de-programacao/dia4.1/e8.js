f1c96c0862d8d83b980d89282195ca3b1e50dd96
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-13 22:18:14 -0300

    Arrumei e8

--- fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/e8.js
+++ fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/e8.js
@@ -3,7 +3,7 @@ const b = 23;
 const c = 57;
 
 if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
-   console.log("true");
+   console.log(true);
 } else {
-   console.log ("false");
+   console.log (false);
 }

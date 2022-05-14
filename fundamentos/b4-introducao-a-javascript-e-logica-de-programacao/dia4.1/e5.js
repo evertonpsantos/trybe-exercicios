bdc7d1e12a5950f3be245513b2999b790a9d06e6
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-13 21:42:58 -0300

    Arrumando exercício 5

--- fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/e5.js
+++ fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/e5.js
@@ -3,9 +3,9 @@ const angle2 = 40;
 const angle3 = 120;
 
 if (angle1 + angle2 + angle3 == 180) {
-  console.log("true");
+  console.log(true);
 } else if (angle1 < 0 || angle2 < 0 || angle3 < 0) {
   console.log("Erro");
 } else {
-  console.log("false");
+  console.log(false);
 }

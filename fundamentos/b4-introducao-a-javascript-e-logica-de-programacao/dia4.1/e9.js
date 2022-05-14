b93182828fd606339194f81d4ed9561271edf878
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-13 22:16:32 -0300

    Arrumando e9

--- fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/e9.js
+++ fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/dia4.1/e9.js
@@ -3,7 +3,7 @@ const b = 232;
 const c = 56;
 
 if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
-   console.log("true");
+   console.log(true);
 } else {
-   console.log ("false");
+   console.log (false);
 }

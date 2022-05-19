51f52a8f2dcf177f499d7572f2b2ffe99ce7a89b
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-05-19 18:50:58 -0300

    Mudando cor de Urgent e Not Urgent

---
+++ fundamentos/b5-dom-eventos-web-storage/dia5.1/script.js
@@ -0,0 +1,13 @@
+let bgHeaderUrgent = document.querySelector(".emergency-tasks");
+bgHeaderUrgent.style.background = "#FFA07A";
+
+let bgHeaderUrgentH3 = document.querySelectorAll(".emergency-tasks h3");
+bgHeaderUrgentH3[0].style.background = "#D116EA";
+bgHeaderUrgentH3[1].style.background = "#D116EA";
+
+let bgHeaderNotUrgent = document.querySelector('.no-emergency-tasks');
+bgHeaderNotUrgent.style.background = "#ECE494";
+
+let bgHeaderNotUrgentH3 = document.querySelectorAll ('.no-emergency-tasks h3');
+bgHeaderNotUrgentH3[0].style.background = "black";
+bgHeaderNotUrgentH3[1].style.background = "black";

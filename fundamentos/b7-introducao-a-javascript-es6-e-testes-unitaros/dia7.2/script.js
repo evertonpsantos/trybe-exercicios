d088676e861d3851d19e18c3f0c5639193d3c65b
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-09 19:22:27 -0300

    Parte: 2 e 3

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script.js
@@ -1,11 +1,21 @@
-function sum() {
+const checkTwoInputs = (value1, value2) => {
+  if (value1.length === 0 || value2.length === 0) {
+    throw new Error(`Digite um número em cada campo!`);
+  }
+}
+
+const isItNan =  (param1, param2) => {
+  if (isNaN(param1) || isNaN(param2)) {
+    throw new Error(`Digite somente números!`)
+  }
+}
+
+const sum = () => {
   let value1 = document.getElementById('value1').value;
   let value2 = document.getElementById('value2').value;
-  
   try {
-    if (value1.length === 0 || value2.length === 0) {
-      throw new Error(`Digite um número em cada campo!`);
-    }
+    isItNan(value1, value2);
+    checkTwoInputs(value1, value2);
     const result = parseInt(value1) + parseInt(value2);
     document.getElementById('result').innerText = `Resultado: ${result}`;
   } catch (error) {

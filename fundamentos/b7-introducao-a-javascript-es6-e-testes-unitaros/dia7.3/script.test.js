aef6d20ef0bd2e0d408eb28d1cd38625f37d87d7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-10 20:13:27 -0300

    Exercício 2

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.test.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.test.js
@@ -1,4 +1,4 @@
-const sum = require('./script')
+const {sum, myRemove} = require('./script')
 
 describe('The sum function', () => {
   it('returns 9 when sum(4, 5)', () => {
@@ -11,3 +11,15 @@ describe('The sum function', () => {
     expect(() => {sum(4, "5")}).toThrowError('parameters must be numbers');
   })
 });
+
+describe('The myRemove function', () => {
+  it('returns [1, 2, 4] when myRemove([1, 2, 3, 4], 3)', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  })
+  it('not returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 3)', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+  it('returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 5)', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
+})

91796921e75c0c1770a22efaf35b531b0708bdb6
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-10 20:34:36 -0300

    Exercício 3

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.test.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./script')
+const {sum, myRemove, myFizzBuzz} = require('./script')
 
 describe('The sum function', () => {
   it('returns 9 when sum(4, 5)', () => {
@@ -21,5 +21,23 @@ describe('The myRemove function', () => {
   })
   it('returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 5)', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
+})
+
+describe('The myFizzBuzz function', () => {
+  it('returns fizzbuzz when myFizzBuzz(15)', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+  })
+  it('returns fizz when myFizzBuzz(9)', () => {
+    expect(myFizzBuzz(9)).toEqual('fizz');
+  })
+  it('returns buzz when myFizzBuzz(25)', () => {
+    expect(myFizzBuzz(25)).toEqual('buzz');
+  })
+  it('returns 2 when myFizzBuzz(2)', () => {
+    expect(myFizzBuzz(2)).toEqual(2);
+  })
+  it('returns false when myFizzBuzz("cinco")', () => {
+    expect(myFizzBuzz("cinco")).toEqual(false);
   })
 })

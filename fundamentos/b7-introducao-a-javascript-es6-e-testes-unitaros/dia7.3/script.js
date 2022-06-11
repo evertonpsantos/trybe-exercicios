50cbaa683f4c30fd067e4d2bdd19ab6fb89f13c7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-10 22:01:50 -0300

    Exercícios dia 7.3

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.js
@@ -1,6 +1,6 @@
 function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("parameters must be numbers");
   }
 
   return a + b;
@@ -17,66 +17,66 @@ function myRemove(arr, item) {
 }
 
 function myFizzBuzz(num) {
-  if (typeof num !== 'number') return false;
-  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
-  if (num % 3 === 0) return 'fizz';
-  if (num % 5 === 0) return 'buzz';
+  if (typeof num !== "number") return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
   return num;
 }
 
 function encode(stringParam) {
-  let splitWord = stringParam.split('');
+  let splitWord = stringParam.split("");
   let aux = [];
   for (let i = 0; i < splitWord.length; i += 1) {
-    if (splitWord[i] === 'a') {
-      aux.push('1');
-    } else if (splitWord[i] === 'e') {
-      aux.push('2');
-    } else if (splitWord[i] === 'i') {
-      aux.push('3');
-    } else if (splitWord[i] === 'o') {
-      aux.push('4');
-    } else if (splitWord[i] === 'u') {
-      aux.push('5');
+    if (splitWord[i] === "a") {
+      aux.push("1");
+    } else if (splitWord[i] === "e") {
+      aux.push("2");
+    } else if (splitWord[i] === "i") {
+      aux.push("3");
+    } else if (splitWord[i] === "o") {
+      aux.push("4");
+    } else if (splitWord[i] === "u") {
+      aux.push("5");
     } else {
       aux.push(splitWord[i]);
     }
   }
-  return aux.join('');
+  return aux.join("");
 }
 
 function decode(stringParam2) {
-  let splitWord = stringParam2.split('');
+  let splitWord = stringParam2.split("");
   let aux = [];
   for (let i = 0; i < splitWord.length; i += 1) {
-    if (splitWord[i] === '1') {
-      aux.push('a');
-    } else if (splitWord[i] === '2') {
-      aux.push('e');
-    } else if (splitWord[i] === '3') {
-      aux.push('i');
-    } else if (splitWord[i] === '4') {
-      aux.push('o');
-    } else if (splitWord[i] === '5') {
-      aux.push('u');
+    if (splitWord[i] === "1") {
+      aux.push("a");
+    } else if (splitWord[i] === "2") {
+      aux.push("e");
+    } else if (splitWord[i] === "3") {
+      aux.push("i");
+    } else if (splitWord[i] === "4") {
+      aux.push("o");
+    } else if (splitWord[i] === "5") {
+      aux.push("u");
     } else {
       aux.push(splitWord[i]);
     }
   }
-  return aux.join('');
+  return aux.join("");
 }
 
 function techList(list, nameEntry) {
   let aux = [];
   let aux2 = list.sort();
   if (list.length < 1) {
-    return 'Vazio!';
+    return "Vazio!";
   }
 
   for (let i = 0; i < aux2.length; i += 1) {
     aux.push({
       tech: aux2[i],
-      name: nameEntry
+      name: nameEntry,
     });
   }
   return aux;
@@ -95,6 +95,14 @@ function hydrate(frase) {
   return `${sum} copos de água`;
 }
 
-console.log(hydrate(`2 cerveja`))
+console.log(hydrate(`2 cerveja`));
 
-module.exports = {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate};
+module.exports = {
+  sum,
+  myRemove,
+  myFizzBuzz,
+  encode,
+  decode,
+  techList,
+  hydrate,
+};
--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.test.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.3/script.test.js
@@ -1,125 +1,143 @@
-const {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate} = require('./script')
+const {
+  sum,
+  myRemove,
+  myFizzBuzz,
+  encode,
+  decode,
+  techList,
+  hydrate,
+} = require("./script");
 
-describe('The sum function', () => {
-  it('returns 9 when sum(4, 5)', () => {
+describe("The sum function", () => {
+  it("returns 9 when sum(4, 5)", () => {
     expect(sum(4, 5)).toEqual(9);
-  })
-  it('returns 0 when sum(0, 0)', () => {
+  });
+  it("returns 0 when sum(0, 0)", () => {
     expect(sum(0, 0)).toEqual(0);
-  })
+  });
   it('throws error when sum(4, "5")', () => {
-    expect(() => {sum(4, "5")}).toThrowError('parameters must be numbers');
-  })
+    expect(() => {
+      sum(4, "5");
+    }).toThrowError("parameters must be numbers");
+  });
 });
 
-describe('The myRemove function', () => {
-  it('returns [1, 2, 4] when myRemove([1, 2, 3, 4], 3)', () => {
+describe("The myRemove function", () => {
+  it("returns [1, 2, 4] when myRemove([1, 2, 3, 4], 3)", () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-  })
-  it('not returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 3)', () => {
+  });
+  it("not returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 3)", () => {
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-  })
-  it('returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 5)', () => {
+  });
+  it("returns [1, 2, 3, 4] when myRemove([1, 2, 3, 4], 5)", () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-  })
-})
+  });
+});
 
-describe('The myFizzBuzz function', () => {
-  it('returns fizzbuzz when myFizzBuzz(15)', () => {
-    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
-  })
-  it('returns fizz when myFizzBuzz(9)', () => {
-    expect(myFizzBuzz(9)).toEqual('fizz');
-  })
-  it('returns buzz when myFizzBuzz(25)', () => {
-    expect(myFizzBuzz(25)).toEqual('buzz');
-  })
-  it('returns 2 when myFizzBuzz(2)', () => {
+describe("The myFizzBuzz function", () => {
+  it("returns fizzbuzz when myFizzBuzz(15)", () => {
+    expect(myFizzBuzz(15)).toEqual("fizzbuzz");
+  });
+  it("returns fizz when myFizzBuzz(9)", () => {
+    expect(myFizzBuzz(9)).toEqual("fizz");
+  });
+  it("returns buzz when myFizzBuzz(25)", () => {
+    expect(myFizzBuzz(25)).toEqual("buzz");
+  });
+  it("returns 2 when myFizzBuzz(2)", () => {
     expect(myFizzBuzz(2)).toEqual(2);
-  })
+  });
   it('returns false when myFizzBuzz("cinco")', () => {
     expect(myFizzBuzz("cinco")).toEqual(false);
-  })
+  });
 });
 
-describe('The encode function', () => {
-  it('is a function', () => {
-    expect(typeof encode).toEqual('function');
-  })
+describe("The encode function", () => {
+  it("is a function", () => {
+    expect(typeof encode).toEqual("function");
+  });
   it('returns 12345 when encode("aeiou")', () => {
-    expect(encode("aeiou")).toEqual('12345')
-  })
+    expect(encode("aeiou")).toEqual("12345");
+  });
   it('returns A5rél34 when encode("Aurélio")', () => {
-    expect(encode("Aurélio")).toEqual('A5rél34');
-  })
-  it('returns string of same length as parameter', () => {
-    expect(encode('4v4').length).toEqual(3);
-  })
+    expect(encode("Aurélio")).toEqual("A5rél34");
+  });
+  it("returns string of same length as parameter", () => {
+    expect(encode("4v4").length).toEqual(3);
+  });
 });
 
-describe('The decode function', () => {
-  it('is a function', () => {
-    expect(typeof decode).toEqual('function');
-  })
+describe("The decode function", () => {
+  it("is a function", () => {
+    expect(typeof decode).toEqual("function");
+  });
   it('returns aeiou when decode("12345")', () => {
-    expect(decode('12345')).toEqual('aeiou');
-  })
+    expect(decode("12345")).toEqual("aeiou");
+  });
   it('returns sequoia when decode("s2q5431")', () => {
-    expect(decode('s2q5431')).toEqual('sequoia');
-  })
-  it('returns string of same length as parameter', () => {
-    expect(encode('ovo').length).toEqual(3);
-  })
+    expect(decode("s2q5431")).toEqual("sequoia");
+  });
+  it("returns string of same length as parameter", () => {
+    expect(encode("ovo").length).toEqual(3);
+  });
 });
 
-describe('Testa a função techList', () => {
-  it('Testa se a função techList é definida', () => {
+describe("Testa a função techList", () => {
+  it("Testa se a função techList é definida", () => {
     expect(techList).toBeDefined();
   });
-  it('Testa se techList é uma função', () => {
-    expect(typeof techList).toBe('function');
+  it("Testa se techList é uma função", () => {
+    expect(typeof techList).toBe("function");
   });
-  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
-    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+  it("Lista com 5 tecnologias deve retornar uma lista de objetos ordenados", () => {
+    expect(
+      techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas")
+    ).toEqual([
       {
-        tech: 'CSS',
-        name: 'Lucas'
+        tech: "CSS",
+        name: "Lucas",
       },
       {
-        tech: 'HTML',
-        name: 'Lucas'
+        tech: "HTML",
+        name: "Lucas",
       },
       {
-        tech: 'JavaScript',
-        name: 'Lucas'
+        tech: "JavaScript",
+        name: "Lucas",
       },
       {
-        tech: 'Jest',
-        name: 'Lucas'
+        tech: "Jest",
+        name: "Lucas",
       },
       {
-        tech: 'React',
-        name: 'Lucas'
-      }
+        tech: "React",
+        name: "Lucas",
+      },
     ]);
   });
   it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
-    expect(techList([], 'Lucas')).toBe('Vazio!');
+    expect(techList([], "Lucas")).toBe("Vazio!");
   });
 });
 
-describe('Testa a função hydrate', () => {
-  it('Testa se a função hydrate é definida', () => {
+describe("Testa a função hydrate", () => {
+  it("Testa se a função hydrate é definida", () => {
     expect(hydrate).toBeDefined();
   });
-  it('Testa se hydrate é uma função', () => {
-    expect(typeof hydrate).toBe('function');
+  it("Testa se hydrate é uma função", () => {
+    expect(typeof hydrate).toBe("function");
   });
-  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
-    expect(hydrate('1 cerveja')).toBe('1 copo de água');
-    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
-    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
-    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
-    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  it("Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber", () => {
+    expect(hydrate("1 cerveja")).toBe("1 copo de água");
+    expect(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho")).toBe(
+      "7 copos de água"
+    );
+    expect(hydrate("2 shots de tequila, 2 cervejas e 1 corote")).toBe(
+      "5 copos de água"
+    );
+    expect(hydrate("1 copo de catuaba, 1 cervejas e 1 copo de vinho")).toBe(
+      "3 copos de água"
+    );
+    expect(hydrate("4 caipirinhas e 2 cervejas")).toBe("6 copos de água");
   });
 });

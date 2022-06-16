d4e60d4291c9c04033464bf7db54f8a6e7b66290
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-16 20:47:52 -0300

    Exercício 5

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script3.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script3.js
@@ -1,27 +1,27 @@
 const lesson1 = {
-  materia: 'Matemática',
+  materia: "Matemática",
   numeroEstudantes: 20,
-  professor: 'Maria Clara',
-  turno: 'manhã',
+  professor: "Maria Clara",
+  turno: "manhã",
 };
 
 const lesson2 = {
-  materia: 'História',
+  materia: "História",
   numeroEstudantes: 20,
-  professor: 'Carlos',
+  professor: "Carlos",
 };
 
 const lesson3 = {
-  materia: 'Matemática',
+  materia: "Matemática",
   numeroEstudantes: 10,
-  professor: 'Maria Clara',
-  turno: 'noite',
+  professor: "Maria Clara",
+  turno: "noite",
 };
 
 // Exercício 1
-const addNightShift = (obj, key, value) => obj[key] = value;
+const addNightShift = (obj, key, value) => (obj[key] = value);
 
-addNightShift(lesson2, 'turno', 'noite');
+addNightShift(lesson2, "turno", "noite");
 
 // Exercício 2
 const listKeys = (obj) => Object.keys(obj);
@@ -31,3 +31,6 @@ const objLength = (obj) => Object.keys(obj).length;
 
 // Exercício 4
 const objValues = (obj) => Object.values(obj);
+
+// Exercício 5
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});

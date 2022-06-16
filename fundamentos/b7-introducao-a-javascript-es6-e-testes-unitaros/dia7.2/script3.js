cdbd476c84789fc9b27fd0b64d7b853da1c0f033
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-16 20:25:00 -0300

    Exercício 1 e 2

---
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script3.js
@@ -0,0 +1,29 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Exercício 1
+const addNightShift = (obj, key, value) => obj[key] = value;
+
+addNightShift(lesson2, 'turno', 'noite');
+
+// Exercício 2
+const listKeys = (obj) => Object.keys(obj);
+
+

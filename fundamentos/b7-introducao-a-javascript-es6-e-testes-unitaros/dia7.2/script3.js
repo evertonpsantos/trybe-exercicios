ff0d8f79a9c3c93dd7a25d15448489e2f085842a
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-16 21:03:29 -0300

    Exercício 6

--- fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script3.js
+++ fundamentos/b7-introducao-a-javascript-es6-e-testes-unitaros/dia7.2/script3.js
@@ -34,3 +34,14 @@ const objValues = (obj) => Object.values(obj);
 
 // Exercício 5
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+// Exercício 6
+const studentNumber = (obj) => {
+  let studentCount = 0;
+  const lessons = Object.keys(obj);
+  for (let i in lessons) {
+    studentCount += obj[lessons[i]]['numeroEstudantes'];
+  }
+  return studentCount;
+};
+console.log(studentNumber(allLessons));

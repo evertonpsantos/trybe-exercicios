3be230d7e30e0eedbd854a0febf8261520cb7680
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-22 16:13:08 -0300

    Exercício 1

---
+++ fundamentos/b8-HOFS/dia8.1/script.js
@@ -0,0 +1,17 @@
+// Exercício 1
+const newPerson = (nomeCompleto) => {
+  const emailGenerator = nomeCompleto.toLowerCase().split(" ").join("_")
+  const newPerson = { nomeCompleto, 
+    email: `${emailGenerator}@trybe.com`
+  };
+  return newPerson
+}
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva') // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};

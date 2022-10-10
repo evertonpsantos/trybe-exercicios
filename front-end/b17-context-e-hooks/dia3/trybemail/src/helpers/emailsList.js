1ff83b09aaa2e9457841b6bbc42f4d0cb65d46f3
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-10 13:33:44 -0300

    Exercício de Docker dia 02

---
+++ front-end/b17-context-e-hooks/dia3/trybemail/src/helpers/emailsList.js
@@ -0,0 +1,13 @@
+export const emailList = [{
+  id: 0,
+  title: 'Cobrança das Casas Bahia',
+  status: 0,
+}, {
+  id: 1,
+  title: 'Viage com Trivago',
+  status: 0,
+}, {
+  id: 2,
+  title: 'EBANX',
+  status: 0,
+}];

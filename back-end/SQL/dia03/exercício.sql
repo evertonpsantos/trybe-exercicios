9dd47b94fe743fcb79d02170ba7c89e2e59c1c13
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-19 13:37:29 -0300

    Exercício 7 :technologist:

--- back-end/SQL/dia03/exercício.sql
+++ back-end/SQL/dia03/exercício.sql
@@ -28,3 +28,7 @@ WHERE code LIKE '%F%';
 SELECT * FROM Fornecimentos
 WHERE Preco BETWEEN 15 AND 40
 ORDER BY Preco;
+
+--Exercício 7
+SELECT COUNT(*) FROM Vendas
+WHERE order_date BETWEEN '2018-04-15' AND '2019-07-30';

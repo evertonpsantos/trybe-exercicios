60ef8a694bd7c2de51edb7e718b762ee9ae5f7c3
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-18 21:48:47 -0300

    Exercício 4 :weight_lifting:

--- back-end/SQL/dia04/exercises.sql
+++ back-end/SQL/dia04/exercises.sql
@@ -16,3 +16,8 @@ VALUES (9, 6.8, 450000000, 370000000);
 UPDATE Movies
 SET director = 'Andrew Staton'
 WHERE title = 'Procurando Nemo';
+
+--Exercício 4
+UPDATE Movies
+SET title = 'Ratatouille', `year` = 2007
+WHERE id = 3;

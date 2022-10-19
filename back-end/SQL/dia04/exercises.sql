f6abe9eaf61af2c896334d39fc9953e8f4845f8e
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-18 21:47:10 -0300

    Exercício 1 :weight_lifting:

---
+++ back-end/SQL/dia04/exercises.sql
@@ -0,0 +1,9 @@
+USE Pixar;
+
+--Exercício 1
+INSERT INTO Movies (title, director, `year`, length_minutes)
+VALUES 
+('Monstros SA', 'Pete Docter', 2001, 92),
+('Procurando Nemo', 'John Lasseter', 2003, 107),
+('Os Incriveis', 'Brad Bird', 2004, 116),
+('WALL-E', 'Pete Docker', 2008, 104);

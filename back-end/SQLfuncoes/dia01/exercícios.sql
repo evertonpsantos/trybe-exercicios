31ed12a9ad2e94201d851fd7d82f9065a20ec1ce
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-20 20:07:01 -0300

    Exercício 4 :sparkle:

--- back-end/SQLfuncoes/dia01/exercícios.sql
+++ back-end/SQLfuncoes/dia01/exercícios.sql
@@ -4,3 +4,12 @@ SELECT MAX(salary) FROM hr.employees;
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário
 SELECT MAX(salary) - MIN(salary) FROM hr.employees;
 
+-- 3. Escreva uma query que exiba a média salarial de cada JOB_ID, ordenando pela média salarial em ordem decrescente.
+SELECT job_id, ROUND(AVG(salary)) AS media_salarial
+FROM hr.employees 
+GROUP BY job_id
+ORDER BY media_salarial DESC;
+
+-- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
+SELECT SUM(salary) FROM hr.employees;
+

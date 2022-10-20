8502b4d38e86f4d570dc6b3540de5aea534acb5e
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-20 20:05:57 -0300

    Exercícios 2 :sparkle:

--- back-end/SQLfuncoes/dia01/exercícios.sql
+++ back-end/SQLfuncoes/dia01/exercícios.sql
@@ -1,3 +1,6 @@
 -- 1. Escreva uma query que exiba o maior salário da tabela.
 SELECT MAX(salary) FROM hr.employees;
 
+-- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário
+SELECT MAX(salary) - MIN(salary) FROM hr.employees;
+

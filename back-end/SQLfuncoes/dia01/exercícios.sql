1727da120c3c37a6a06eb266c10f5bd97a190f0b
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-20 20:13:49 -0300

    Exercício 14 :sparkle:

--- back-end/SQLfuncoes/dia01/exercícios.sql
+++ back-end/SQLfuncoes/dia01/exercícios.sql
@@ -63,3 +63,8 @@ WHERE CHAR_LENGTH(first_name) >= 8;
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e ano no qual foi contratado (exiba somente o ano).
 SELECT employee_id, first_name, YEAR(hire_date) as hired
 FROM hr.employees;
+
+-- 14. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
+SELECT employee_id, first_name, DAY(hire_date) as hire_day
+FROM hr.employees;
+

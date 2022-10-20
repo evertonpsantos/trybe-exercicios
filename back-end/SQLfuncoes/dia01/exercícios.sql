0f5e287c679070d77967fc903eb2eaa2a885f01e
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-20 20:07:40 -0300

    Exercício 5 :sparkle:

--- back-end/SQLfuncoes/dia01/exercícios.sql
+++ back-end/SQLfuncoes/dia01/exercícios.sql
@@ -13,3 +13,10 @@ ORDER BY media_salarial DESC;
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
 SELECT SUM(salary) FROM hr.employees;
 
+-- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
+SELECT 
+	ROUND(MAX(salary), 2) AS biggest_salary,
+	ROUND(MIN(salary), 2) AS smallest_salary,
+    	ROUND(SUM(salary), 2) AS salary_sum,
+	ROUND(AVG(salary), 2) AS salary_average
+FROM hr.employees;

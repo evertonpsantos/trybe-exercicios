dbd369bb745de7474311203b04e54b5065934816
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-20 20:09:27 -0300

    Exercício 8 :sparkle:

--- back-end/SQLfuncoes/dia01/exercícios.sql
+++ back-end/SQLfuncoes/dia01/exercícios.sql
@@ -30,3 +30,9 @@ WHERE job_id = 'IT_PROG';
 SELECT job_id, SUM(salary) as total
 FROM hr.employees
 GROUP BY job_id;
+
+-- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras (IT_PROG).
+SELECT job_id, SUM(salary) as total
+FROM hr.employees
+GROUP BY job_id
+HAVING job_id = 'IT_PROG';

9fba6542f6ab4139e0689e83415d6073c63ff778
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-21 20:16:46 -0300

    Exercício 2 :bank:

--- back-end/SQLfuncoes/dia02/exercises.sql
+++ back-end/SQLfuncoes/dia02/exercises.sql
@@ -7,3 +7,15 @@ FROM
 	pixar.movies AS p
 		INNER JOIN 
 	pixar.box_office AS BO ON p.id = BO.movie_id;
+
+-- Exercício 2
+SELECT 
+    p.title,
+    (BO.domestic_sales + BO.international_sales) AS total_sales
+FROM
+    pixar.movies AS p
+        INNER JOIN
+    pixar.box_office AS BO ON p.id = BO.movie_id
+WHERE
+    BO.international_sales > BO.domestic_sales
+ORDER BY total_sales DESC;

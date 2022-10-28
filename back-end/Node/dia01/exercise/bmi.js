29f721ae1b81f45c7b4b54b602a6dd7861c39655
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-28 12:32:39 -0300

    Exercício 3 e adiciona .gitignore

--- back-end/Node/dia01/exercise/bmi.js
+++ back-end/Node/dia01/exercise/bmi.js
@@ -1,10 +1,14 @@
+const readline = require('readline-sync');
+
 const calculateBMI = (weight, heightInCm) => {
   const newHeight = heightInCm / 100;
   return (weight / (newHeight * newHeight)).toFixed(2);
 };
 
 const main = () => {
-  const bmi = calculateBMI(95, 160);
+  const weight = readline.question("What's your weight (in kg)? \n")
+  const height = readline.question("What's your height (in cm)? \n")
+  const bmi = calculateBMI(weight, height);
   console.log(`Your BMI is ${bmi}`);
 };
 

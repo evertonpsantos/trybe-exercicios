92e053e87b1a2397076e220d99565003398ec4e7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-28 12:48:03 -0300

    Exercício 5

--- back-end/Node/dia01/exercise/bmi.js
+++ back-end/Node/dia01/exercise/bmi.js
@@ -9,9 +9,12 @@ const main = () => {
   const weight = readline.questionFloat("What's your weight (in kg)? \n")
   const height = readline.questionInt("What's your height (in cm)? \n")
   const bmi = calculateBMI(weight, height);
-  console.log(`Your BMI is ${bmi}`);
+  if (bmi < 18.5) console.log(`You're really thin. Your BMI is ${bmi}`);
+  if (bmi >= 18.5 && bmi <= 24.9) console.log(`You have a normal weight. Your BMI is ${bmi}`);
+  if (bmi >= 25 && bmi <= 29.9) console.log(`You're overweight. Your BMI is ${bmi}`);
+  if (bmi >= 30 && bmi <= 34.9) console.log(`You're obese level I. Your BMI is ${bmi}`);
+  if (bmi >= 35 && bmi <= 39.9) console.log(`You're obese level II. Your BMI is ${bmi}`);
+  if (bmi >= 40) console.log(`You're obese level III. Your BMI is ${bmi}`);
 };
 
 main();
-
-//Adiciona questionFloat

d76a9c727ac7503e5bc9237ecb5f5f73d24426b5
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-28 12:27:44 -0300

    Exercício 2

--- back-end/Node/dia01/exercise/bmi.js
+++ back-end/Node/dia01/exercise/bmi.js
@@ -2,3 +2,10 @@ const calculateBMI = (weight, heightInCm) => {
   const newHeight = heightInCm / 100;
   return (weight / (newHeight * newHeight)).toFixed(2);
 };
+
+const main = () => {
+  const bmi = calculateBMI(95, 160);
+  console.log(`Your BMI is ${bmi}`);
+};
+
+main();

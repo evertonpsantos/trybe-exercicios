3ba21d8b06d054f0a1897ab0fe0070feec133f2a
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-11-01 19:44:06 -0300

    Cria middleware de validação de nome

--- back-end/Node/dia04/exercise/src/app.js
+++ back-end/Node/dia04/exercise/src/app.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post('/activities', (req, res) => {
+const validateName = (req, res, next) => {
+  if (!('name' in req.body)) {
+    return res.status(400).json({ message: 'O campo name é obrigatório.' });
+  }
+  if (req.body.name.length < 5) {
+    return res.status(400).json({ message: 'O campo name precisa ter mais de 4 letras.' });
+  }
+  next();
+}
+
+app.post('/activities', validateName, (req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso' });
 });
 

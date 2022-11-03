01175969bf75f73b79e1851b3d820e79ce87f4ef
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-11-03 13:57:51 -0300

    Exercício 8

--- back-end/Node/dia02/exercise/src/app.js
+++ back-end/Node/dia02/exercise/src/app.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 
 const moviesPath = path.resolve(__dirname, './movies.json');
 
@@ -10,6 +11,15 @@ async function readJson() {
   try {
     const data = await fs.readFile(moviesPath);
     return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+}
+
+async function writeJson(newMovie) {
+  try {
+    await fs.writeFile(moviesPath, newMovie);
+    return newMovie;
   } catch (error) {
     console.error(`Arquivo não pôde ser lido: ${error}`);
   }
@@ -25,6 +35,15 @@ app.get('/movies/:id', async (req, res) => {
   const movies = await readJson();
   const chosenMovie = movies.find((movie) => movie.id === Number(paramId));
   res.status(200).json(chosenMovie);
+});
+
+app.post('/movies', async (req, res) => {
+  const movies = await readJson();
+  const lastPosition = movies.length - 1;
+  const newMovie = { id: movies[lastPosition].id + 1, ...req.body };
+  const newList = JSON.stringify([...movies, newMovie]);
+  await writeJson(newList);
+  res.status(201).json(newMovie);
 });
 
 async function main() {

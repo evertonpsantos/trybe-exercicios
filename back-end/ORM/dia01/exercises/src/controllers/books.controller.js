9161bb81eacd51fbad89ca4c5b1d35fa9da497f2
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-11-23 00:05:09 -0300

    Exercício 7

--- back-end/ORM/dia01/exercises/src/controllers/books.controller.js
+++ back-end/ORM/dia01/exercises/src/controllers/books.controller.js
@@ -5,7 +5,14 @@ const getAll = async (_req, res) => {
   return res.status(200).json(books);
 };
 
+const getById = async (req, res) => {
+  const bookId = req.params.id;
+  const foundBook = await BookService.getById(bookId);
+  if (foundBook === undefined) return res.status(404).json({ message: "Book Not Found"});
+  return res.status(200).json(foundBook);
+};
 
 module.exports = {
   getAll,
+  getById,
 }

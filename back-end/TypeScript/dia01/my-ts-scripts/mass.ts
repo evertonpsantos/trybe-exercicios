2ddf744e1bb0bc7a94bcf8ae653578e029564ee1
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-12-13 13:01:03 -0300

    Exercício 3

--- back-end/TypeScript/dia01/my-ts-scripts/mass.ts
+++ back-end/TypeScript/dia01/my-ts-scripts/mass.ts
@@ -1,11 +1,8 @@
-const units = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
+export const units = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
 
-function convert(value: number, baseUni: string, conversionUni: string):number {
-  const fromIndex = units.indexOf(baseUni); // pegamos o index da unidade base no array
-  const toIndex = units.indexOf(conversionUni); // pegamos o index da unidade para a conversão
-  console.log(fromIndex)
-  console.log(toIndex)
-  const exponent = toIndex - fromIndex; // calculamos o expoente a partir da diferença dos index
-  console.log(exponent);
+export function convert(value: number, baseUni: string, conversionUni: string):number {
+  const fromIndex = units.indexOf(baseUni);
+  const toIndex = units.indexOf(conversionUni);
+  const exponent = toIndex - fromIndex;
   return value * Math.pow(10, exponent);
 }

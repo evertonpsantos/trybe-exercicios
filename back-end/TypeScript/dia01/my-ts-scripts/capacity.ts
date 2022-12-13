0b7cac2d38b23d7b7cfee54b949084c6a0076f0c
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-12-13 13:33:59 -0300

    Exercício 7 :man_technologist:

--- back-end/TypeScript/dia01/my-ts-scripts/capacity.ts
+++ back-end/TypeScript/dia01/my-ts-scripts/capacity.ts
@@ -1,3 +1,5 @@
+import readline from 'readline-sync';
+
 export const units = ['kl', 'hl', 'dal', 'l', 'dl', 'cl', 'ml'];
 
 export function convert(value: number, baseUni: string, conversionUni: string):number {
@@ -6,3 +8,24 @@ export function convert(value: number, baseUni: string, conversionUni: string):n
   const exponent = toIndex - fromIndex;
   return value * Math.pow(10, exponent);
 }
+
+export function exec() {
+  const value = readline.questionFloat('Digite o valor a ser convertido: \n');
+  const fromUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para a unidade base:');
+  const toUnitChoiceIndex = readline.keyInSelect(units, 'Escolha um número para a conversão:');
+
+  const toUnitChoice = units[toUnitChoiceIndex];
+  const fromUnitChoice = units[fromUnitChoiceIndex];
+
+  if (!fromUnitChoice || !toUnitChoice) {
+    console.log(`Função cancelada`);
+    return 0;
+  }
+  
+  const result = convert(value, fromUnitChoice, toUnitChoice);
+  const message = `${value}${fromUnitChoice} é igual a ${result}${toUnitChoice}`;
+
+  console.log(message);
+}
+
+exec()

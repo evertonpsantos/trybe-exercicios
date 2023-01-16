2a3c486105aa9d2299e7ead1072300d1a5545597
evertonpsantos <evertonpsantos8@gmail.com>
2023-01-16 12:53:01 -0300

    Exercício 2 :man_technologist:

--- back-end/POO-SOLID/dia01/Student.ts
+++ back-end/POO-SOLID/dia01/Student.ts
@@ -4,20 +4,28 @@ class Student {
 
     constructor(private _name: string, private _registration: string) {}
 
-    public get name () {
+    public get name() {
         return this._name;
     }
 
-    public get registration () {
+    public get registration() {
         return this._registration;
     }
 
-    public get examNotes () {
+    public get examNotes() {
         return this._examNotes;
     }
 
-    public get workNotes () {
+    public get workNotes() {
         return this._workNotes;
+    }
+
+    public set name(name: string) {
+        this._name = name;
+    }
+
+    public set registration(registration: string) {
+        this._registration = registration;
     }
 
     public set newExamNotes (newNotes: number[]) {
@@ -29,10 +37,22 @@ class Student {
     }
 
     public set newWorkNotes (newNotes: number[]) {
-        if (newNotes.length !== 4) {
+        if (newNotes.length !== 2) {
             throw new Error('Student must have 2 work notes');
         }
 
         this._workNotes = newNotes;
+    }
+
+    sumNotes() {
+        const examNotes = this._examNotes.reduce((acc, curr) => acc + curr);
+        const workNotes = this._workNotes.reduce((acc, curr) => acc + curr);
+        return examNotes + workNotes;
+    }
+
+    getAverage() {
+        const notes = this.sumNotes();
+        const notesQuantity = this._examNotes.length + this._workNotes.length;
+        return Math.round(notes / notesQuantity);
     }
 }

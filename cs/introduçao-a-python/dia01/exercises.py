9e8e0ed3c1fd09db0d4b17c75c6c515e69331059
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-23 13:59:55 -0300

    feat: exercício 4

--- cs/introduçao-a-python/dia01/exercises.py
+++ cs/introduçao-a-python/dia01/exercises.py
@@ -17,10 +17,25 @@ def calculate_average(list):
         list_value_sim += value
     return list_value_sim // list_length
 
+
 # Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1,
 # imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
 
-def printSquare(number):
+def print_square(number):
     for _ in range(number):
-        print('*' * number)
+        print("*" * number)
+
+
+# Exercício 4: Crie uma função que receba uma lista de nomes
+# e retorne o nome com a maior quantidade de caracteres.
+# Por exemplo, para ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"],
+# o retorno deve ser "Fernanda".
+
+
+def check_bigger_name(name_list):
+    bigger_name = ""
+    for name in name_list:
+        if len(name) > len(bigger_name):
+            bigger_name = name
+    return bigger_name

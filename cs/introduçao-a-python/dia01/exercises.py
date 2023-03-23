ff6ea6b4446bcad343570eea651160bc0ca90389
evertonpsantos <evertonpsantos8@gmail.com>
2023-03-23 13:50:23 -0300

    feat: exercise 3

--- cs/introduçao-a-python/dia01/exercises.py
+++ cs/introduçao-a-python/dia01/exercises.py
@@ -1,9 +1,11 @@
 # Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
 
+
 def biggest_number(number1, number2):
     if number1 > number2:
         return number1
     return number2
+
 
 # Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
 
@@ -14,3 +16,11 @@ def calculate_average(list):
     for value in list:
         list_value_sim += value
     return list_value_sim // list_length
+
+# Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1,
+# imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+
+
+def printSquare(number):
+    for _ in range(number):
+        print('*' * number)

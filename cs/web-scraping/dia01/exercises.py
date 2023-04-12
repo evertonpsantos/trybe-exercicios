7d26250b9927981ec3e2ad1a5bb3d6de54a0d1c8
evertonpsantos <evertonpsantos8@gmail.com>
2023-04-12 13:26:30 -0300

    exercício 4

--- cs/web-scraping/dia01/exercises.py
+++ cs/web-scraping/dia01/exercises.py
@@ -1,6 +1,5 @@
 import requests
-
-# from parsel import Selector
+from parsel import Selector
 
 # Exercício 1
 
@@ -16,8 +15,22 @@ import requests
 
 # Exercício 3
 
+# response = requests.get(
+#     "https://www.scrapethissite.com/pages/advanced/?gotcha=headers", headers={
+#       'Accept': 'text/html',
+#       'User-Agent': 'Mozilla'})
+# assert "bot detected" not in response.text
+
+# Exercício 4
+
 response = requests.get(
-    "https://www.scrapethissite.com/pages/advanced/?gotcha=headers", headers={
-      'Accept': 'text/html',
-      'User-Agent': 'Mozilla'})
-assert "bot detected" not in response.text
+    "http://books.toscrape.com/catalogue/the-grand-design_405/index.html"
+)
+selector = Selector(response.text)
+title = selector.css("h1::text").get()
+price = selector.css(".price_color::text").re_first(r"\d+\.\d{2}")
+description = (
+    selector.css("#product_description ~ p::text").get().removesuffix("...more")
+)
+image_url = selector.css("img::attr(src)").get()
+print(f"{title}, {price}, {description}, {image_url}")
